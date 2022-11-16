56af4e5a65ec34cea6b7b82032fc273155fd86fc
weston <weston.agreda@gmail.com>
2022-11-16 14:45:35 -0500

    AbilityID gone
    my c++ brain took over, realized these are useless in JS

--- js/marbleClap.js
+++ js/marbleClap.js
@@ -13,10 +13,9 @@ var Deck = /** @class */ (function () {
     return Deck;
 }());
 var Lane = /** @class */ (function () {
-    function Lane(name, description, abilityID) {
+    function Lane(name, description) {
         this.name = name;
         this.description = description;
-        this.abilityID = abilityID;
     }
     Lane.prototype.destory = function () {
     };
@@ -25,7 +24,7 @@ var Lane = /** @class */ (function () {
     return Lane;
 }());
 var Card = /** @class */ (function () {
-    function Card(name, cost, power, description, abilityType, abilityID, pool) {
+    function Card(name, cost, power, description, abilityType, pool) {
         this.name = name;
         this.cost = cost;
         this.power = power;
@@ -33,7 +32,6 @@ var Card = /** @class */ (function () {
         this.originalPower = power;
         this.description = description;
         this.abilityType = abilityType;
-        this.abilityID = abilityID;
         this.pool = pool;
     }
     Card.prototype.play = function (lane) {
@@ -61,7 +59,7 @@ function generateLanes(locationsJSON, whichLocations) {
     for (var i = 0; i < whichLocations.length; i++) {
         var currentLocation = locationsJSON[Object.keys(locationsJSON)[whichLocations[i]]];
         console.log(currentLocation.name);
-        lanes.push(new Lane(currentLocation.name, currentLocation.description, currentLocation.abilityID));
+        lanes.push(new Lane(currentLocation.name, currentLocation.description));
     }
     console.log(lanes);
 }
@@ -75,14 +73,14 @@ function generateCards(cardsJSON, whichCards) {
     for (var i = 0; i < whichCards.length; i++) {
         var currentCard = cardsJSON[Object.keys(cardsJSON)[whichCards[i]]]; // this is confusing and weird just trust it works cause it does
         console.log(currentCard.name); //testing that it does work
-        cards.push(new Card(currentCard.name, currentCard.cost, currentCard.power, currentCard.description, currentCard.abilityType, currentCard.abilityID, currentCard.pool));
+        cards.push(new Card(currentCard.name, currentCard.cost, currentCard.power, currentCard.description, currentCard.abilityType, currentCard.pool));
     }
     ;
     console.log(cards); // shows the generated cards, pogu
 }
 function runAbility(card) {
-    switch (card.abilityID) {
-        case 1:
+    switch (card.name) {
+        case "Mister Fantastic":
             //runs mister fantastic's ability :)
             //*psuedocode 
             //if card.lane is the middle then add two power to left and right lanes
@@ -90,11 +88,10 @@ function runAbility(card) {
             //store the value 2 in a variable so it can be more easily doubled by onslaught etc.
             console.log("mister fantastic ability ran");
             break;
-        case 2:
+        case "Quicksilver":
             //runs someone else's ability
             console.log("Someone else ability epic");
         default:
-            //runs when the abilityID is -1 or something has broken
             console.log("Nice job ya broke it");
             break;
     }
--- typescript/marbleClap.ts
+++ typescript/marbleClap.ts
@@ -23,12 +23,10 @@ class Lane { //Primarily just holds values and which cards are in the lane
     playerCards: Card[];
     opponentPower: number;
     playerPower: number;
-    abilityID: number; //abilityID is -1 if no ability or if ability is meaningless to the puzzle (like central park)
 
-    constructor(name: string, description: string, abilityID: number){
+    constructor(name: string, description: string){
         this.name = name;
         this.description = description;
-        this.abilityID = abilityID;
     }
 
     destory(){ //Just for puzzles with Galactus for now
@@ -47,12 +45,11 @@ class Card {
     originalPower: number;
     description: string;
     abilityType: string; //ongoing, onReveal, other, or none !!other and none are not the same!! *patriot* 
-    abilityID: number; //abilityID just a unique number for later usage, just an integer for later use, still -1 for no ability
     posistion: number; //position is -1 if the card is in a hand and not on the board, 0-4 to describe where in its lane
     pool: number;
     currentLane: Lane;
 
-    constructor(name: string, cost: number, power: number, description: string, abilityType: string, abilityID: number, pool: number){
+    constructor(name: string, cost: number, power: number, description: string, abilityType: string, pool: number){
         this.name = name;
         this.cost = cost;
         this.power = power;
@@ -60,7 +57,6 @@ class Card {
         this.originalPower = power;
         this.description = description;
         this.abilityType = abilityType;
-        this.abilityID = abilityID;
         this.pool = pool;
     }
 
@@ -102,7 +98,7 @@ function generateLanes(locationsJSON, whichLocations){
     for(let i = 0; i < whichLocations.length; i++){
         let currentLocation = locationsJSON[Object.keys(locationsJSON)[whichLocations[i]]];
         console.log(currentLocation.name);
-        lanes.push(new Lane(currentLocation.name, currentLocation.description, currentLocation.abilityID));
+        lanes.push(new Lane(currentLocation.name, currentLocation.description));
     }
     console.log(lanes);
 }
@@ -118,14 +114,14 @@ function generateCards(cardsJSON, whichCards){
     for (let i = 0; i < whichCards.length; i++) {
         let currentCard = cardsJSON[Object.keys(cardsJSON)[whichCards[i]]]; // this is confusing and weird just trust it works cause it does
         console.log(currentCard.name); //testing that it does work
-        cards.push(new Card(currentCard.name, currentCard.cost, currentCard.power, currentCard.description, currentCard.abilityType, currentCard.abilityID, currentCard.pool));
+        cards.push(new Card(currentCard.name, currentCard.cost, currentCard.power, currentCard.description, currentCard.abilityType, currentCard.pool));
     };
     console.log(cards); // shows the generated cards, pogu
 }
 
 function runAbility(card: Card){
-    switch (card.abilityID) {
-        case 1:
+    switch (card.name) {
+        case "Mister Fantastic":
             //runs mister fantastic's ability :)
             //*psuedocode 
             //if card.lane is the middle then add two power to left and right lanes
@@ -133,11 +129,10 @@ function runAbility(card: Card){
             //store the value 2 in a variable so it can be more easily doubled by onslaught etc.
             console.log("mister fantastic ability ran");
             break;
-        case 2:
+        case "Quicksilver":
             //runs someone else's ability
             console.log("Someone else ability epic");
         default:
-            //runs when the abilityID is -1 or something has broken
             console.log("Nice job ya broke it");
             break;
     }
